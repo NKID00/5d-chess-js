15e406a128e30ae61e1a69f372870acd8e448db6
SnowmanFactory <cuberryan@gmail.com>
2021-07-17 13:14:28 -0700

    fully refactored action.js

--- src/action.js
+++ src/action.js
@@ -1,77 +1,76 @@
 const boardFuncs = require('@local/board');
 
-exports.actions = (board, actionNum, activeOnly = true, presentOnly = true, newActiveTimelinesOnly = true, variant = 'standard', promotionPieces = null) => {
-  var recurse =  (board, actionNum, layer = 0, totalMoves = 0, totalLayers = 0, totalIndex = []) => {
-    var returnArr = [];
-    var moves = boardFuncs.moves(board, actionNum, activeOnly, presentOnly, promotionPieces);
-    for(var i = 0;i < moves.length;i++) {
-      var moddedBoard = boardFuncs.copy(board);
-      if(
-        !newActiveTimelinesOnly ||
-        (newActiveTimelinesOnly && (this.newTimelineIsActive(moddedBoard, actionNum) || (!this.newTimelineIsActive(moddedBoard, actionNum) && boardFuncs.positionIsLatest(moddedBoard, moves[i][1]))))
-      ) {
-        boardFuncs.move(moddedBoard, moves[i]);
-        totalMoves++;
-        if(boardFuncs.present(moddedBoard, actionNum).length > 0) {
-          totalIndex[layer] = i;
-          var recurseRes = recurse(moddedBoard, actionNum, layer + 1, totalMoves, totalLayers, totalIndex);
-          var nextLayer = recurseRes[0];
-          totalMoves = recurseRes[1];
-          totalLayers = recurseRes[2] > layer ? recurseRes[2] : layer;
-          totalIndex = recurseRes[3];
-          for(var j = 0;j < nextLayer.length;j++) {
-            var currArr = [moves[i]];
-            for(var k = 0;k < nextLayer[j].length;k++) {
-              currArr.push(nextLayer[j][k]);
-            }
-            returnArr.push(currArr);
-          }
-          if(nextLayer.length === 0) {
-            returnArr.push([moves[i]]);
-          }
+exports.actions = (fullBoard, actionNum, activeOnly = true, presentOnly = true, newActiveTimelinesOnly = true, promotionPieces = null) => {
+
+  let recurse = (board, actionNum, layer = 0, totalMoves = 0, totalLayers = 0, totalIndex = []) => {
+    const moves = boardFuncs.moves(board, actionNum, activeOnly, presentOnly, promotionPieces);
+    let res = [];
+
+    for (let i = 0; i < moves.length; i++) {
+      const moddedBoard = boardFuncs.copy(board);
+      const isNewTimelineActive = this.newTimelineIsActive(moddedBoard, actionNum)
+
+      if (newActiveTimelinesOnly && (!newActiveTimelinesOnly || (!isNewTimelineActive && (isNewTimelineActive || !boardFuncs.positionIsLatest(moddedBoard, moves[i][1]))))) continue;
+
+      totalMoves++;
+
+      if (boardFuncs.present(moddedBoard, actionNum).length > 0) {
+        totalIndex[layer] = i;
+        const recurseRes = recurse(moddedBoard, actionNum, layer + 1, totalMoves, totalLayers, totalIndex);
+        const nextLayer = recurseRes[0];
+
+        totalMoves = recurseRes[1];
+        totalLayers = recurseRes[2] > layer ? recurseRes[2] : layer;
+        totalIndex = recurseRes[3];
+
+        for (const layer of nextLayer) {
+          let currArr = [moves[i]];
+
+          for (const item of layer) currArr.push(item);
+
+          res.push(currArr);
         }
-        else {
-          returnArr.push([moves[i]]);
-        }
+
+        if (nextLayer.length == 0) res.push([moves[i]]);
       }
+      else res.push([moves[i]]);
     }
-    return [returnArr, totalMoves, totalLayers, totalIndex];
+    return [res, totalMoves, totalLayers, totalIndex];
   }
-  return recurse(board, actionNum)[0];
+  return recurse(fullBoard, actionNum)[0];
 }
 
-exports.move = (board, moves) => {
-  for(var i = 0;i < moves.length;i++) {
-    boardFuncs.move(board, moves[i]);
-  }
+exports.move = (fullBoard, moves) => {
+  for (const move of moves) boardFuncs.move(fullBoard, move);
 }
 
-exports.newTimelineIsActive = (board, actionNum) => {
-  var minTimeline = 0;
-  var maxTimeline = 0;
-  for(var l = 0;board && l < board.length;l++) {
-    if(board[l]) {
-      if(minTimeline < l && l % 2 !== 0) {
-        minTimeline = l;
-      }
-      if(maxTimeline < l && l % 2 === 0) {
-        maxTimeline = l;
-      }
-    }
+exports.newTimelineIsActive = (fullBoard, actionNum) => {
+  let minTimeline = 0;
+  let maxTimeline = 0;
+
+  for (let l = 0; fullBoard && l < fullBoard.length; l++) {
+
+    if (!fullBoard[l]) continue;
+
+    minTimeline = (minTimeline < l && l % 2 != 0) ? l : minTimeline;
+
+    maxTimeline = (maxTimeline < l && l % 2 == 0) ? l : maxTimeline;
+
   }
-  if(actionNum % 2 === 0) {
-    return (minTimeline + 1) >= maxTimeline;
-  }
+
+  if (actionNum % 2 == 0) return (minTimeline + 1) >= maxTimeline;
+
   return (minTimeline + 1) <= maxTimeline;
 }
 
 exports.databaseHasBoard = (board, db) => {
-  for(var i = 0;i < db.length;i++) {
-    var comparison = boardFuncs.compare(board, db[i]);
-    if(comparison === 0) {
-      return true;
-    }
+  for (let i = 0; i < db.length; i++) {
+    const comparison = boardFuncs.compare(board, db[i]);
+
+    if (comparison == 0) return true;
   }
+
   db.push(boardFuncs.copy(board));
+
   return false;
 }
