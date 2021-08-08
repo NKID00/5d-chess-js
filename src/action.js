8ce3df2ca0ee75023f91b44e5c841ea213c85199
SnowmanFactory <cuberryan@gmail.com>
2021-08-08 13:42:02 -0700

    Fixed a Stack Limit Error

--- src/action.js
+++ src/action.js
@@ -2,20 +2,21 @@ const boardFuncs = require('@local/board');
 
 exports.actions = (fullBoard, actionNum, activeOnly = true, presentOnly = true, newActiveTimelinesOnly = true, promotionPieces = null) => {
 
-  let recurse = (board, actionNum, layer = 0, totalMoves = 0, totalLayers = 0, totalIndex = []) => {
-    const moves = boardFuncs.moves(board, actionNum, activeOnly, presentOnly, promotionPieces);
-    let res = [];
+  const recurse = (fullBoard, actionNum, layer = 0, totalMoves = 0, totalLayers = 0, totalIndex = []) => {
+    const moves = boardFuncs.moves(fullBoard, actionNum, activeOnly, presentOnly, promotionPieces);
+    let returnArr = [];
 
     for (let i = 0; i < moves.length; i++) {
-      const moddedBoard = boardFuncs.copy(board);
-      const isNewTimelineActive = this.newTimelineIsActive(moddedBoard, actionNum)
+      const moddedBoard = boardFuncs.copy(fullBoard);
 
-      if (newActiveTimelinesOnly && (!newActiveTimelinesOnly || (!isNewTimelineActive && (isNewTimelineActive || !boardFuncs.positionIsLatest(moddedBoard, moves[i][1]))))) continue;
+      if (newActiveTimelinesOnly && (!newActiveTimelinesOnly || (!this.newTimelineIsActive(moddedBoard, actionNum) && (this.newTimelineIsActive(moddedBoard, actionNum) || !boardFuncs.positionIsLatest(moddedBoard, moves[i][1]))))) continue;
 
+      boardFuncs.move(moddedBoard, moves[i]);
       totalMoves++;
 
       if (boardFuncs.present(moddedBoard, actionNum).length > 0) {
         totalIndex[layer] = i;
+
         const recurseRes = recurse(moddedBoard, actionNum, layer + 1, totalMoves, totalLayers, totalIndex);
         const nextLayer = recurseRes[0];
 
@@ -23,25 +24,38 @@ exports.actions = (fullBoard, actionNum, activeOnly = true, presentOnly = true, 
         totalLayers = recurseRes[2] > layer ? recurseRes[2] : layer;
         totalIndex = recurseRes[3];
 
-        for (const layer of nextLayer) {
+        for (let j = 0; j < nextLayer.length; j++) {
           let currArr = [moves[i]];
 
-          for (const item of layer) currArr.push(item);
+          for (let k = 0; k < nextLayer[j].length; k++) {
 
-          res.push(currArr);
+            currArr.push(nextLayer[j][k]);
+
+          }
+
+          returnArr.push(currArr);
         }
 
-        if (nextLayer.length == 0) res.push([moves[i]]);
+        if (nextLayer.length === 0) returnArr.push([moves[i]]);
+
       }
-      else res.push([moves[i]]);
+      else {
+
+        returnArr.push([moves[i]]);
+      }
+
     }
-    return [res, totalMoves, totalLayers, totalIndex];
+
+    return [returnArr, totalMoves, totalLayers, totalIndex];
   }
+
   return recurse(fullBoard, actionNum)[0];
 }
 
 exports.move = (fullBoard, moves) => {
-  for (const move of moves) boardFuncs.move(fullBoard, move);
+  for (const move of moves) {
+    boardFuncs.move(fullBoard, move);
+  }
 }
 
 exports.newTimelineIsActive = (fullBoard, actionNum) => {
@@ -52,25 +66,25 @@ exports.newTimelineIsActive = (fullBoard, actionNum) => {
 
     if (!fullBoard[l]) continue;
 
-    minTimeline = (minTimeline < l && l % 2 != 0) ? l : minTimeline;
+    if (minTimeline < l && l % 2 !== 0) minTimeline = l;
 
-    maxTimeline = (maxTimeline < l && l % 2 == 0) ? l : maxTimeline;
+    if (maxTimeline < l && l % 2 === 0) maxTimeline = l;
 
   }
 
-  if (actionNum % 2 == 0) return (minTimeline + 1) >= maxTimeline;
+  if (actionNum % 2 === 0) return (minTimeline + 1) >= maxTimeline;
 
   return (minTimeline + 1) <= maxTimeline;
 }
 
-exports.databaseHasBoard = (board, db) => {
+exports.databaseHasBoard = (fullBoard, db) => {
   for (let i = 0; i < db.length; i++) {
-    const comparison = boardFuncs.compare(board, db[i]);
+    const comparison = boardFuncs.compare(fullBoard, db[i]);
 
     if (comparison == 0) return true;
+
   }
 
-  db.push(boardFuncs.copy(board));
-
+  db.push(boardFuncs.copy(fullBoard));
   return false;
 }
