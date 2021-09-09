0dd9eaf4fb6d699c4ce15d87e084579a7db19053
SnowmanFactory <cuberryan@gmail.com>
2021-09-08 18:43:51 -0700

    Fully refactored hash.js

--- src/hash.js
+++ src/hash.js
@@ -1,74 +1,101 @@
 const md5 = require('blueimp-md5');
 
-exports.hash = (board) => {
-  var res = '';
-  for(var l = 0;board && l < board.length;l++) {
-    if(board[l]) {
-      for(var t = 0;t < board[l].length;t++) {
-        if(board[l][t]) {
-          var zeroSum = 0;
-          for(var r = 0;r < board[l][t].length;r++) {
-            for(var f = 0;f < board[l][t][r].length;f++) {
-              var piece = board[l][t][r][f];
-              if(piece === 0) {
+exports.hash = (fullBoard) => {
+  let res = '';
+
+  for (const timeline of fullBoard) {
+
+    if (timeline) {
+
+      for (const turn of timeline) {
+
+        if (turn) {
+          let zeroSum = 0;
+
+          for (const rank of turn) {
+            for (const file of rank) {
+              const piece = file;
+
+              if (piece == 0) {
+
                 zeroSum++;
-              }
-              else {
-                if(zeroSum > 0) {
+
+              } else {
+
+                if (zeroSum > 0) {
+
                   res += '' + zeroSum;
+
                   zeroSum = 0;
+
                 }
-                if(piece > 0) {
+
+                if (piece > 0) {
+
                   res += String.fromCharCode(97 + piece);
-                }
-                else {
+
+                } else {
+
                   res += String.fromCharCode(67 + Math.abs(piece));
                 }
               }
             }
           }
-        }
-        else {
+        } else {
+
           res += 'B';
         }
       }
-    }
-    else {
+    } else {
+
       res += 'A';
     }
   }
+
   return md5(res);
 }
 
-exports.hashBoard = (board, timeline, turn) => {
-  var res = '';
-  var l = timeline;
-  var t = turn;
-  if(board[l][t]) {
-    var zeroSum = 0;
-    for(var r = 0;r < board[l][t].length;r++) {
-      for(var f = 0;f < board[l][t][r].length;f++) {
-        var piece = board[l][t][r][f];
-        if(piece === 0) {
+exports.hashBoard = (fullBoard, timeline, turn) => {
+  const l = timeline;
+  const t = turn;
+  let res = '';
+
+  if (fullBoard[l][t]) {
+    let zeroSum = 0;
+
+    for (const rank of fullBoard[l][t]) {
+      for (const file of rank) {
+        const piece = file;
+
+        if (piece == 0) {
+
           zeroSum++;
-        }
-        else {
-          if(zeroSum > 0) {
+
+        } else {
+
+          if (zeroSum > 0) {
+
             res += '' + zeroSum;
+
             zeroSum = 0;
+
           }
-          if(piece > 0) {
+
+          if (piece > 0) {
+
             res += String.fromCharCode(97 + piece);
-          }
-          else {
+
+          } else {
+
             res += String.fromCharCode(67 + Math.abs(piece));
           }
         }
       }
     }
-  }
-  else {
+  } else {
+
     res += 'B';
   }
+
   return md5(res);
 }
